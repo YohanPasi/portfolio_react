58c536797be372ab57b39d3c69afb54f3698382b
Yohan <yohanpasi80253327@gmail.com>
2024-12-09 22:55:05 +0530

    nav bar & Hero Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {darkTheme} from "./utils/thems"
+import '../src/App.css';
+import styled, {ThemeProvider} from 'styled-components';
 
+const Body = styled.div`
+  background-color: ${({theme}) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  `;
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+   <ThemeProvider theme={darkTheme}>
+    <Body>Portfolio</Body>
+   </ThemeProvider>
   );
 }
 
