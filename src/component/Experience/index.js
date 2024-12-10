0ae012ffb21d5c7fafa5e7f94b6049d4506b5dea
Yohan <yohanpasi80253327@gmail.com>
2024-12-11 01:45:44 +0530

    Make the changes to data.js

--- src/component/Experience/index.js
+++ src/component/Experience/index.js
@@ -80,7 +80,7 @@ const index = () => {
             <Wrapper>
                 <Title>Experience</Title>
                 <Desc>
-                    My work experience as a software engineer and working on different companies and projects.
+                My experience by working on diverse projects and roles, developing skills in organization, problem-solving, and teamwork.
                 </Desc>
                 <TimelineSection>
                     <Timeline>
